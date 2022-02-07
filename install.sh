ed94b4de3e8f833fa796d4eda017b4a0a3e3427f
lunz1207 <lunz1207@gmail.com>
2022-02-07 18:52:31 +0800

    ok

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 helm install grafana bitnami/grafana
 helm install prometheus prometheus-community/prometheus
 
-
-admin
-oAYyqTS368
+admin / oAYyqTS368
+podName=k get pod | grep -Eo 'jmeter-master-.*\z'
