0ab4c947c47bd34f69052a7cd07327d4341641ae
lunz1207 <lunz1207@gmail.com>
2022-02-17 18:13:38 +0800

    upgrade install

--- install.sh
+++ install.sh
@@ -1,19 +1,15 @@
-# 创建集群
-echo "------------------- 创建 k8s 集群 --------------------"
-kind create cluster --config kind.yaml
-
 # 安装被测 app
-echo "------------------- 安装 pythonapp --------------------"
+echo "------------------- 安装 api service --------------------"
 kubectl apply -f service/demo/deployment.yaml
 
 # 安装jmeter
-echo "------------------- 安装 jmeter --------------------"
+echo "------------------- 安装 jmeter cluster --------------------"
 kubectl apply -f jmeter/deployment.yaml
 
 # 安装 prometheus
-# echo "------------------- 安装 prometheus --------------------"
+echo "------------------- 安装 prometheus --------------------"
 # helm install prometheus prometheus-community/prometheus
-# helm install prometheus -f deployment/values-prometheus.yaml prometheus-community/prometheus
+helm install prometheus -f deployment/values-prometheus.yaml prometheus-community/prometheus
 
 # 安装 influxdb
 echo "------------------- 安装 influxdb --------------------"
@@ -23,8 +19,10 @@ helm install influxdb bitnami/influxdb
 echo "------------------- 安装 grafana --------------------"
 helm install grafana bitnami/grafana
 
-echo "User: admin"
-echo "Password: $(kubectl get secret grafana-admin --namespace default -o jsonpath="{.data.GF_SECURITY_ADMIN_PASSWORD}" | base64 --decode)"
+echo "Grafana User: admin"
+echo "Grafana Password: $(kubectl get secret grafana-admin --namespace default -o jsonpath="{.data.GF_SECURITY_ADMIN_PASSWORD}" | base64 --decode)"
+
+echo "------------------- install  done --------------------"
 
 # .5 grafana
 #admin / oAYyqTS368
