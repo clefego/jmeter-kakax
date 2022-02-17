0eb78e7cc7a85a9463a70df617fe5f95871225a4
lunz1207 <lunz1207@gmail.com>
2022-02-17 17:33:05 +0800

    chore install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 echo "------------------- 创建 k8s 集群 --------------------"
 kind create cluster --config kind.yaml
 
-
 # 安装被测 app
 echo "------------------- 安装 pythonapp --------------------"
 kubectl apply -f service/demo/deployment.yaml
@@ -11,12 +10,14 @@ kubectl apply -f service/demo/deployment.yaml
 echo "------------------- 安装 jmeter --------------------"
 kubectl apply -f jmeter/deployment.yaml
 
-
 # 安装 prometheus
-echo "------------------- 安装 prometheus --------------------"
+# echo "------------------- 安装 prometheus --------------------"
 # helm install prometheus prometheus-community/prometheus
-helm install prometheus -f deployment/values-prometheus.yaml prometheus-community/prometheus
+# helm install prometheus -f deployment/values-prometheus.yaml prometheus-community/prometheus
 
+# 安装 influxdb
+echo "------------------- 安装 influxdb --------------------"
+helm install influxdb bitnami/influxdb
 
 # 安装 grafana
 echo "------------------- 安装 grafana --------------------"
@@ -25,14 +26,11 @@ helm install grafana bitnami/grafana
 echo "User: admin"
 echo "Password: $(kubectl get secret grafana-admin --namespace default -o jsonpath="{.data.GF_SECURITY_ADMIN_PASSWORD}" | base64 --decode)"
 
-
-
-
 # .5 grafana
 #admin / oAYyqTS368
-
-
 
 # 本地  grafana
 # admin / x8QIbrWwWQ
 # http://127.0.0.1:8080/?orgId=1
+
+# 本地 influxdb 3HRVFCqPAr
