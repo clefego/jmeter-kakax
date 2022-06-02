fdef426be3a6f00a88b3924fd85c09dd173a7ab7
lunz1207 <lunz1207@gmail.com>
2022-06-03 01:49:14 +0800

    chore: update readme & scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,6 +18,7 @@ echo "------------------- install influxdb --------------------"
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install influxdb bitnami/influxdb
 
+echo "------------------- Get the admin credentials --------------------"
 echo "User: admin"
 echo "Password: $(kubectl get secret influxdb --namespace default -o jsonpath="{.data.admin-user-password}" | base64 --decode)"
 echo "Token: $(kubectl get secret influxdb --namespace default -o jsonpath="{.data.admin-user-token}" | base64 --decode)"
@@ -26,16 +27,4 @@ echo "Token: $(kubectl get secret influxdb --namespace default -o jsonpath="{.da
 echo "------------------- install grafana --------------------"
 helm install grafana bitnami/grafana
 
-echo "Grafana User: admin"
-echo "Grafana Password: $(kubectl get secret grafana-admin --namespace default -o jsonpath="{.data.GF_SECURITY_ADMIN_PASSWORD}" | base64 --decode)"
-
 echo "------------------- install  done --------------------"
-
-# .5 grafana
-#admin / oAYyqTS368
-
-# 本地  grafana
-# admin / x8QIbrWwWQ
-# http://127.0.0.1:8080/?orgId=1
-
-# 本地 influxdb 3HRVFCqPAr
