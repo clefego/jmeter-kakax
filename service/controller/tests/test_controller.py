1d1198674ecea76aa3d847aa79c3f8d986de0eba
lunz1207 <lunz1207@gmail.com>
2022-06-03 23:17:01 +0800

    feat: controller testcase

--- service/controller/tests/test_controller.py
+++ service/controller/tests/test_controller.py
@@ -1,10 +1,30 @@
+from pprint import pprint
+import uuid
+
 from requests_toolbelt.sessions import BaseUrlSession
 
 
 class TestController():
 
-    bs = BaseUrlSession(base_url='http://127.0.0.1:8003')
+    bs = BaseUrlSession(base_url='http://127.0.0.1:8000')
 
     def test_root(self):
         resp = self.bs.get('/')
+        assert resp.status_code == 200
+
+    def test_create_job(self):
+        resp = self.bs.post(f'/jobs/jmx')
+        pprint(resp.json())
+        assert resp.status_code == 200
+
+    def test_get_job(self):
+        resp = self.bs.get(f'/jobs/jmx')
+        assert resp.status_code == 200
+
+    def test_updata_job(self):
+        resp = self.bs.patch(f'/jobs/jmx')
+        assert resp.status_code == 200
+
+    def test_delete_job(self):
+        resp = self.bs.delete(f'/jobs/jmx')
         assert resp.status_code == 200
